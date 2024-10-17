50bdcb2cd6d80161b3021e6e81e6f5fa59a43152
rem1776 <Ryan.Mulhall@noaa.gov>
2024-10-17 14:58:37 -0400

    add test and platform for ci

--- fre/tests/test_fre_make_cli.py
+++ fre/tests/test_fre_make_cli.py
@@ -20,3 +20,8 @@ def test_cli_fre_make_opt_dne():
     ''' fre make optionDNE '''
     result = runner.invoke(fre.fre, args=["make", "optionDNE"])
     assert result.exit_code == 2
+
+def test_cli_fre_make_null_model_build_baremetal():
+    ''' fre make --help '''
+    result = runner.invoke(fre.fre, args=["make", "run-fremake", "-y", "/fre-cli/fre/make/tests/null_example/null_model.yaml", "-p", "ci.gnu", "-t", "debug"])
+    assert result.exit_code == 0
