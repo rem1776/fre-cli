be4c94846478a6689a937e17beb08bfbaddf8978
rem1776 <Ryan.Mulhall@noaa.gov>
2024-10-22 16:51:27 -0400

    add mkmf and update paths

---
+++ fre/make/tests/test_fre_make_run_fremake.py
@@ -0,0 +1,12 @@
+''' test "fre make" calls '''
+
+from click.testing import CliRunner
+
+from fre import fre
+
+runner = CliRunner()
+
+def test_fre_make_run_fremake_null_model():
+    ''' run fre make with run-fremake subcommand and build the null model experiment with gnu'''
+    result = runner.invoke(fre.fre, args=["make", "run-fremake", "-y", "/fre-cli/fre/make/tests/null_example/null_model.yaml", "-p", "ci.gnu", "-t", "debug"])
+    assert result.exit_code == 0
--- fre/tests/test_fre_make_cli.py
+++ fre/tests/test_fre_make_cli.py
@@ -21,7 +21,3 @@ def test_cli_fre_make_opt_dne():
     result = runner.invoke(fre.fre, args=["make", "optionDNE"])
     assert result.exit_code == 2
 
-def test_cli_fre_make_null_model_build_baremetal():
-    ''' fre make --help '''
-    result = runner.invoke(fre.fre, args=["make", "run-fremake", "-y", "/fre-cli/fre/make/tests/null_example/null_model.yaml", "-p", "ci.gnu", "-t", "debug"])
-    assert result.exit_code == 0
