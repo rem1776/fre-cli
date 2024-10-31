67f5c2e30d2075f3effe01064e8d028af585dec2
rem1776 <Ryan.Mulhall@noaa.gov>
2024-10-31 15:09:23 -0400

    adjust yaml path for test

--- fre/make/tests/test_fre_make_run_fremake.py
+++ fre/make/tests/test_fre_make_run_fremake.py
@@ -6,7 +6,8 @@ from fre import fre
 
 runner = CliRunner()
 
+## assumes you're running pytest from repo root dir
 def test_fre_make_run_fremake_null_model():
     ''' run fre make with run-fremake subcommand and build the null model experiment with gnu'''
-    result = runner.invoke(fre.fre, args=["make", "run-fremake", "-y", "/fre-cli/fre/make/tests/null_example/null_model.yaml", "-p", "ci.gnu", "-t", "debug"])
+    result = runner.invoke(fre.fre, args=["make", "run-fremake", "-y", "fre/make/tests/null_example/null_model.yaml", "-p", "ci.gnu", "-t", "debug"])
     assert result.exit_code == 0
